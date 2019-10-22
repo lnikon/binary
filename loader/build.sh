be43d73651277101a685ad7d49fa9f3f5aec81f8
lnikon <nreplay@list.ru>
2019-10-22 15:37:17 +0200

    working on tool main

---
+++ loader/build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 main.cc ../inc/loader.cc -o main -lbfd
---
+++ loader/main.cc
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+#include "../inc/loader.h"
+
+void
+usage(char** argv)
+{
+    if(argv == nullptr) 
+    {
+        return;
+    }
+
+    printf("Usage: %s <binary>\n", argv[0]);
+}
+
+int
+main(int argc, char** argv)
+{
+    if(argc != 2)
+    {
+        usage(argv);
+        exit(1);
+    }
+
+    std::string binaryPath = argv[1];
+    Binary bin;
+    if(load_binary(binaryPath, &bin, Binary::BinaryType::BIN_TYPE_AUTO) < 0)
+    {
+        printf("Error loading binary\n");
+        exit(1);
+    }
+
+    printf("Loaded binary: %s %s %s (%u bits) entry@0x%016jx\n",
+            bin.filename, 
+            bin.type_str.c_str(), bin.arch_str.c_str(),
+            bin.bits, bin.entry);
+    
+    return 0;
+}
